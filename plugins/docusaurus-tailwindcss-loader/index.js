f2c1ae220662520e3c23f3b7d53f2d3dbfd8862f
Rajiv Seelam <rajiv@betalectic.com>
2024-10-05 12:02:28 +0530

    Sample docusarus website

---
+++ plugins/docusaurus-tailwindcss-loader/index.js
@@ -0,0 +1,36 @@
+module.exports = function (context, options) {
+  return {
+    name: "postcss-tailwindcss-loader",
+    // https://github.com/facebook/docusaurus/issues/2961#issuecomment-1531243979
+    injectHtmlTags() {
+      return {
+        headTags: [
+          {
+            tagName: "link",
+            attributes: {
+              rel: "stylesheet",
+              href: "https://cdn.jsdelivr.net/npm/tailwindcss/dist/preflight.min.css",
+            },
+          },
+        ],
+      };
+    },
+    configurePostCss(postcssOptions) {
+      // Appends new PostCSS plugin.
+      //   postcssOptions.plugins.push(
+      //     require("postcss-import"),
+      //     require("tailwindcss"),
+      //     require("postcss-preset-env")({
+      //       autoprefixer: {
+      //         flexbox: "no-2009",
+      //       },
+      //       stage: 4,
+      //     })
+      //   );
+      // https://github.com/facebook/docusaurus/issues/2961#issuecomment-1531243979
+      postcssOptions.plugins.push(require("tailwindcss"));
+      postcssOptions.plugins.push(require("autoprefixer"));
+      return postcssOptions;
+    },
+  };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
+  corePlugins: { preflight: false },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
