79418af9317a09887a321c6f4f517aec5c4fbe5b
Rajiv Seelam <rajiv@betalectic.com>
2024-10-05 17:21:52 +0530

    Make changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://rjvim.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "rjvim", // Usually your GitHub org/user name.
+  projectName: "landing", // Usually your repo name.
+  trailingSlash: false,
   plugins: ["docusaurus-tailwindcss-loader"],
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
