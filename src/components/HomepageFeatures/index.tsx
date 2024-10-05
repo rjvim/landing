f22204e1978188517c2513d098838d559d0f6a65
Rajiv Seelam <rajiv@betalectic.com>
2024-10-05 17:46:46 +0530

    Change strings

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,12 +10,12 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Easy to Use",
+    title: "Easy to Use...",
     Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        used to get your website up and running quickly...
       </>
     ),
   },
