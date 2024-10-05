fb722cdf2e414b33ec641b4d8a989f8f76664a93
Rajiv Seelam <rajiv@betalectic.com>
2024-10-05 17:24:08 +0530

    Fix broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,15 +104,15 @@ const config: Config = {
     footer: {
       style: "dark",
       links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
+        // {
+        //   title: "Docs",
+        //   items: [
+        //     {
+        //       label: "Tutorial",
+        //       to: "/docs/intro",
+        //     },
+        //   ],
+        // },
         {
           title: "Community",
           items: [
