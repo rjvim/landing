5be1794ef1542cb1bccb7c91c53f8fb4f6feec6b
Rajiv Seelam <rajiv@betalectic.com>
2024-10-06 17:36:21 +0530

    Revert changes except footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import Footer from "@theme-original/Footer";
+// import Footer from "@theme-original/Footer";
 import type FooterType from "@theme/Footer";
 import type { WrapperProps } from "@docusaurus/types";
 import { useThemeConfig } from "@docusaurus/theme-common";
+import { Footer } from "@site/src/components/Footer";
 
 type Props = WrapperProps<typeof FooterType>;
 
 export default function FooterWrapper(props: Props): JSX.Element {
-  const { footer } = useThemeConfig();
-  if (!footer) {
-    return null;
-  }
-  const { copyright, links, logo, style } = footer;
-  console.log("footer", footer);
+  // const { footer } = useThemeConfig();
+  // if (!footer) {
+  //   return null;
+  // }
+  // const { copyright, links, logo, style } = footer;
+  // console.log("footer", footer);
 
   return (
     <>
-      <div className="p-3 bg-red-400 dark:bg-yellow-400">
-        {/* <Footer {...props} /> */}
-      </div>
+      <Footer />
     </>
   );
 }
